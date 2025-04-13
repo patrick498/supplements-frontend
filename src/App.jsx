6f100e4fb89d202f3f474afd1ff0504989f035e5
Patrick Pailhes <193191145+patrick498@users.noreply.github.com>
2025-04-13 11:09:44 +0900

    Added Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,9 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600">
+        <h1 className="text-white text-4xl font-bold">Tailwind CSS v4 + Vite + React</h1>
+      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>

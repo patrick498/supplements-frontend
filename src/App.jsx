fc6139176aaf429645835fd4469e5d3a5a3b31dd
Patrick Pailhes <193191145+patrick498@users.noreply.github.com>
2025-04-14 15:53:33 +0900

    Added react-auth-kit

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+// Routes
+import { Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Intakes from './pages/Intakes'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="p-4">
+        {/* Navigation */}
+        <nav className="mb-4 space-x-4">
+          <Link to="/intakes">Intakes</Link>
+        </nav>
+        {/* Routes */}
+        <Routes>
+          <Route path="/intakes" element={<Intakes />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600">
-        <h1 className="text-white text-4xl font-bold">
-          Tailwind CSS v4 + Vite + React
-        </h1>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from 'react-auth-kit'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider
+      authType="localstorage"
+      authName="_auth"
+      cookieDomain={window.location.hostname}
+      cookieSecure={false}
+    >
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 )
